085fce839979fb5cc319f0f7ee3dc83ea5ee3a31
Theeoprs <perustheo2@gmail.com>
2026-05-02 22:09:37 +0200

    (feat) batch fetcher done

---
+++ ingestion/batch_fetcher.py
@@ -0,0 +1,37 @@
+import csv
+import os
+from dotenv import load_dotenv
+from dataclasses import asdict
+from mock_generator import mock_generator 
+from google.cloud import storage 
+
+load_dotenv()
+gen = mock_generator()
+header = ["circulation_id",
+  "ligne_id",
+  "date_circulation",
+  "gare_depart",
+  "gare_arrivee",
+  "heure_depart",
+  "heure_arrivee",
+  "retard",
+  "etat",
+]
+data = []
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+DESTINATION_BLOB = os.getenv("DESTINATION_BLOB")
+client = storage.Client()
+bucket = client.bucket(BUCKET_NAME)
+
+for _ in range (100):
+  circulation = next(gen)
+  data.append(asdict(circulation).values())
+
+with open("circulations.csv", 'w') as f: 
+  writer = csv.writer(f) 
+  writer.writerow(header)
+  writer.writerows(data)
+
+
+blob = bucket.blob(DESTINATION_BLOB)
+blob.upload_from_filename("circulations.csv")
--- ingestion/mock_generator.py
+++ ingestion/mock_generator.py
@@ -1,5 +1,5 @@
 import random
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 import uuid
 import datetime
 import time
