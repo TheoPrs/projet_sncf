2952425209e9bea57432670225c3dc031afe0289
Theeoprs <perustheo2@gmail.com>
2026-05-04 03:11:51 +0200

    (test) add mock_generator unit tests

---
+++ tests/test_mock_generator.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, 'ingestion')
+from mock_generator import mock_generator
+from dataclasses import asdict
+
+
+def test_data_mock_generator():
+    gen = mock_generator()
+    circulation = next(gen)
+    assert circulation.gare_depart != circulation.gare_arrivee
+    assert circulation.etat == "a_l_heure" or circulation.etat == "en_retard"
+    assert circulation.heure_arrivee > circulation.heure_depart
+    for _ in asdict(circulation).values():
+        assert _ is not None
